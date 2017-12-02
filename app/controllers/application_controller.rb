94d6578a95cb3b42a259afdfb1aad59b973388b8
Martin-Alexander <martingianna@gmail.com>
2017-12-02 02:29:48 -0500

    visiting an unauthorized controller action redirects back to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,14 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+
   private
+  
+  def user_not_authorized
+    redirect_to(root_path)
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /^admin/
