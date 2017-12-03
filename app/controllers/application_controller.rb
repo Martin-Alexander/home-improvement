e684024d4fe3350748f83b5c901596d1aef984c7
Martin-Alexander <martingianna@gmail.com>
2017-12-03 04:34:53 -0500

    removed testing method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :test
-
-  def test
-    # byebug
-  end
 
   include Pundit
 
