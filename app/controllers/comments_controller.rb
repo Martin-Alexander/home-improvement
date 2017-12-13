098f96db49720671ddbf4f843b050a744a90de7a
Martin-Alexander <martingianna@gmail.com>
2017-12-12 20:21:18 -0500

    configured CommentsController to handle create requests from react and removed index method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,43 +3,19 @@ class CommentsController < ApplicationController
   before_action :authorize_comment, only: [:update]
   skip_before_action :authenticate_user!, only: [:index]
 
-  def index
-    @project = Project.find(params[:project_id])
-
-    # Only top-level commments of selected project (with safeguard against showing
-    # comments of private projects)
-    project_comments = policy_scope(Comment)
-      .joins(:project).where(comment_id: nil, projects: {private: false}, project: @project)
-
-    case params[:filter]
-
-    # Comments in reverse cronological order
-    when "new"
-      @comments = project_comments.order(created_at: :desc)
-
-    # Comments in order of most likes
-    when "top"
-      @comments = project_comments.select("comments.*, COUNT(*) as like_count")
-        .joins("LEFT JOIN likes ON likes.comment_id = comments.id ")
-        .group("comments.id")
-        .order("like_count DESC")
-
-    # Comments in order of most comments
-    when "active"
-      @comments = project_comments.select("comments.*, COUNT(_comments.*) as comment_count")
-        .joins("LEFT JOIN comments _comments ON _comments.comment_id = comments.id")
-        .group("comments.id")
-        .order("comment_count DESC")
-    end
-  end
-
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
 
     authorize_comment
 
-    @comment.save
+    respond_to do |format|
+      if @comment.save
+        format.json { render json: {status: "OK", comment: @comment.as_json_for_react(current_user)} }
+      else
+        format.json { render json: {status: "FAILURE"} }
+      end
+    end
   end
 
   def update
@@ -63,6 +39,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.permit(:comment_id, :project_id, :content)
+    params.require(:comment).permit(:comment_id, :project_id, :content)
   end
 end
